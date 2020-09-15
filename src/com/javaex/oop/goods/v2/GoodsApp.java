6f3572e678ffd6a71a5e38cff6bf18b18b43a32b
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-09-15 17:17:48 +0900

    객체지향 기초

---
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -0,0 +1,21 @@
+package com.javaex.oop.goods.v2;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		//	사용(참조)
+		Goods notebook = new Goods();
+		//	필드에 접근(우회접근)
+		notebook.setName("LG Gram");
+		notebook.setPrice(1600000);
+		
+		Goods smartphone = new Goods();
+		smartphone.setName("iPhone SE");
+		smartphone.setPrice(600000);
+		
+		//	출력
+		notebook.showInfo();
+		smartphone.showInfo();
+	}
+
+}
