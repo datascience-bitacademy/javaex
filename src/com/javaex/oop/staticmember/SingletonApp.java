09f309947807b4c8d04d824c0dd66586a49f12b7
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-09-15 17:39:41 +0900

    Singleton 예제

--- src/com/javaex/oop/staticmember/SingletonApp.java
+++ src/com/javaex/oop/staticmember/SingletonApp.java
@@ -5,6 +5,8 @@ public class SingletonApp {
 	public static void main(String[] args) {
 //		Singleton s1 = new Singleton();	//	오류: 생성자 private
 		Singleton s1 = Singleton.getInstance();
+		s1.getScores()[5] = 80;
+		
 		Singleton s2 = Singleton.getInstance();
 		
 		System.out.println("s1 = " + s1);
@@ -12,6 +14,7 @@ public class SingletonApp {
 		
 		System.out.println("s1 == s2 ? " + (s1 == s2)); // 두 객체는 동일 객체임
 
+		System.out.println("s2[5] == " + s2.getScores()[5]);
 	}
 
 }
