2fe84b42a5fd91d3156379499920b1cba2aeb31e
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-09-18 15:24:26 +0900

    Network(Echo Server)

---
+++ src/com/javaex/thread/AlphabetThread.java
@@ -0,0 +1,20 @@
+package com.javaex.thread;
+
+public class AlphabetThread implements Runnable {
+
+	@Override
+	public void run() {
+		// 쓰레드 실행 로직
+		for (char ch = 'A'; ch <= 'Z'; ch++) {
+			System.out.println("[AlphabetThread]:" + ch);
+			
+			//	잠시 대기
+			try {
+				Thread.sleep(300); //	0.3 초 지연
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
